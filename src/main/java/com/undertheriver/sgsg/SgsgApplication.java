b7aff1c8f511750989cadbd483cb9d87e7948156
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-03-13 16:16:26 +0900

    init

---
+++ src/main/java/com/undertheriver/sgsg/SgsgApplication.java
@@ -0,0 +1,13 @@
+package com.undertheriver.sgsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgsgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/undertheriver/sgsg/SgsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undertheriver.sgsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
