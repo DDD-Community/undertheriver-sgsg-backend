f075394e6206c1fc5617bd9e81b0ac328889c789
hongbin23 <khbasd@gmail.com>
2021-04-15 23:57:13 +0900

    feat: 로그인 및 유저에필요한 공통 객체 추가

---
+++ src/main/java/com/undertheriver/sgsg/common/dto/CurrentUser.java
@@ -0,0 +1,27 @@
+package com.undertheriver.sgsg.common.dto;
+
+import com.undertheriver.sgsg.common.type.UserRole;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public final class CurrentUser {
+	private Long id;
+	private String name;
+	private String profileImageUrl;
+	private UserRole userRole;
+
+	@Builder
+	private CurrentUser(Long id, String name, String profileImageUrl, UserRole userRole) {
+		this.id = id;
+		this.name = name;
+		this.profileImageUrl = profileImageUrl;
+		this.userRole = userRole;
+	}
+}
