4b2279862b9a7d0cf874979c894c9b5f42520ed9
hongbin23 <khbasd@gmail.com>
2021-04-26 00:10:38 +0900

    test: 회원조회 및 회원탈퇴 인수테스트 작성

---
+++ src/test/java/com/undertheriver/sgsg/user/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.undertheriver.sgsg.user.controller;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.undertheriver.sgsg.acceptance.AcceptanceTest;
+import com.undertheriver.sgsg.user.controller.dto.UserDto;
+
+class UserControllerTest extends AcceptanceTest {
+
+	@DisplayName("유저 정보를 조회한다")
+	@Test
+	void name() {
+		UserDto.DetailResponse response = getOne("/api/v1/users/me", UserDto.DetailResponse.class);
+
+		assertAll(
+			() -> assertThat(response.getName()).isEqualTo("TEST"),
+			() -> assertThat(response.getEmail()).isEqualTo("test@test.com"),
+			() -> assertThat(response.getProfileImageUrl()).isEqualTo("http://naver.com/adf.png")
+		);
+	}
+
+	@DisplayName("회원을 탈퇴한다")
+	@Test
+	public void dummy() {
+		delete("/api/v1/users/me");
+	}
+}
