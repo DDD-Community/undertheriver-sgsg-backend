045b527de5eb28071793f3852472274c1b4dcb62
hongbin23 <khbasd@gmail.com>
2021-04-26 00:16:33 +0900

    test: 테스트 불필요한 코드 제거 및 메서드순서 변경

--- src/test/java/com/undertheriver/sgsg/acceptance/AcceptanceTest.java
+++ src/test/java/com/undertheriver/sgsg/acceptance/AcceptanceTest.java
@@ -1,15 +1,11 @@
 package com.undertheriver.sgsg.acceptance;
 
 import static io.restassured.RestAssured.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -18,7 +14,6 @@ import org.springframework.test.context.jdbc.Sql;
 
 import com.undertheriver.sgsg.auth.common.JwtProvider;
 import com.undertheriver.sgsg.common.type.UserRole;
-import com.undertheriver.sgsg.user.controller.dto.UserDto;
 import com.undertheriver.sgsg.user.domain.User;
 import io.restassured.RestAssured;
 
@@ -34,6 +29,23 @@ public abstract class AcceptanceTest {
 	private JwtProvider jwtProvider;
 
 	private String token;
+
+	@BeforeEach
+	void setUp() {
+		User user = User.builder()
+			.email("test@test.com")
+			.profileImageUrl("http://naver.com/adf.png")
+			.userRole(UserRole.USER)
+			.name("TEST")
+			.userSecretMemoPassword("1234")
+			.build();
+
+		token = jwtProvider.createToken(1L, user.getUserRole());
+
+		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+			RestAssured.port = port;
+		}
+	}
 
 	protected <T> T getOne(String url, Class<T> classType) {
 		//@formatter:off
@@ -88,50 +100,6 @@ public abstract class AcceptanceTest {
 			.log()
 			.all();
 		//@formatter:on
-	}
-
-	@BeforeEach
-	void setUp() {
-		User user = User.builder()
-			.email("test@test.com")
-			.profileImageUrl("http://naver.com/adf.png")
-			.userRole(UserRole.USER)
-			.name("TEST")
-			.userSecretMemoPassword("1234")
-			.build();
-
-		token = jwtProvider.createToken(1L, user.getUserRole());
-
-		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
-			RestAssured.port = port;
-		}
-	}
-
-	@DisplayName("유저 정보 조회")
-	@Test
-	void name() {
-		//@formatter:off
-			System.out.println(token);
-			UserDto.DetailResponse userDetail = given()
-				.when()
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.accept(MediaType.APPLICATION_JSON_VALUE)
-				.auth()
-				.oauth2(token)
-				.get("/api/v1/users/me")
-				.then()
-				.statusCode(HttpStatus.SC_OK)
-				.log()
-				.all()
-				.extract()
-				.as(UserDto.DetailResponse.class);
-			//@formatter:on
-
-		assertAll(
-			() -> assertThat(userDetail.getName()).isEqualTo("TEST"),
-			() -> assertThat(userDetail.getEmail()).isEqualTo("test@test.com"),
-			() -> assertThat(userDetail.getProfileImageUrl()).isEqualTo("http://naver.com/adf.png")
-		);
 	}
 }
 
