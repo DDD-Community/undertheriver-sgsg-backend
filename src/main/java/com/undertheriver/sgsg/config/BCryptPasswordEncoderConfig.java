8525dd27dcad9dfd6c7eb05028f3707749dfbeff
hongbin23 <khbasd@gmail.com>
2021-04-26 22:54:38 +0900

    refactor: POJO 설정파일 적용

--- src/main/java/com/undertheriver/sgsg/config/BCryptPasswordEncoderConfig.java
+++ src/main/java/com/undertheriver/sgsg/config/BCryptPasswordEncoderConfig.java
@@ -3,19 +3,22 @@ package com.undertheriver.sgsg.config;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class BCryptPasswordEncoderConfig {
 
-	@Value("auth.encrypt.seed")
-	private String encryptSeed;
+	private final AppProperties appProperties;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder(4, new SecureRandom(encryptSeed.getBytes(StandardCharsets.UTF_8)));
+		String seed = appProperties.getEncrypt()
+			.getSeed();
+		return new BCryptPasswordEncoder(4, new SecureRandom(seed.getBytes(StandardCharsets.UTF_8)));
 	}
 }
