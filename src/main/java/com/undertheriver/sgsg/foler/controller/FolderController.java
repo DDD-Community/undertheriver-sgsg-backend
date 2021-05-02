04166e42ad1da7383dd04ecbfc2862d85bc29034
plzprayme <dbfpzk142@gmail.com>
2021-05-02 16:58:50 +0900

    fix: 컨트롤러 리턴 타입 응답 공통 모델로 교체

--- src/main/java/com/undertheriver/sgsg/foler/controller/FolderController.java
+++ src/main/java/com/undertheriver/sgsg/foler/controller/FolderController.java
@@ -3,8 +3,8 @@ package com.undertheriver.sgsg.foler.controller;
 import java.net.URI;
 import java.util.List;
 
+import com.undertheriver.sgsg.common.ApiResult;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,47 +25,49 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/folders")
 @Api(value = "folder")
 public class FolderController {
-	private final FolderService folderService;
+    private final FolderService folderService;
 
-	@ApiOperation(value = "폴더 생성")
-	@PostMapping
-	public ResponseEntity<Object> save(
-		@LoginUserId Long userId,
-		@RequestBody FolderDto.CreateFolderReq dto) {
-		try {
-			Long id = folderService.save(userId, dto).getId();
-			URI location = new URI("/api/folders/" + id);
-			return ResponseEntity.created(location)
-				.build();
-		} catch (IndexOutOfBoundsException e) {
-			return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
-				.body(e.getMessage());
-		} catch (Exception e) {
-			return ResponseEntity.badRequest()
-				.body(e.getMessage());
-		}
-	}
+    @ApiOperation(value = "폴더 생성")
+    @PostMapping
+    public ApiResult<?> save(
+            @LoginUserId Long userId,
+            @RequestBody FolderDto.CreateFolderReq dto) {
+        try {
+            Long id = folderService.save(userId, dto).getId();
+            URI location = new URI("/api/folders/" + id);
+            return ApiResult.OK(location);
+        } catch (IndexOutOfBoundsException e) {
+            return ApiResult.ERROR(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception e) {
+            return ApiResult.ERROR(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
-	@ApiOperation("폴더 조회")
-	@GetMapping
-	public ResponseEntity<List<FolderDto.ReadFolderRes>> read(@LoginUserId Long userId) {
-		List<FolderDto.ReadFolderRes> folders = folderService.readAll(userId);
-		return ResponseEntity.ok(folders);
-	}
+    @ApiOperation("폴더 조회")
+    @GetMapping
+    public ApiResult<List<FolderDto.ReadFolderRes>> read(@LoginUserId Long userId) {
 
-	@ApiOperation("폴더 수정")
-	@PutMapping("/{id}")
-	public ResponseEntity<FolderDto.ReadFolderRes> update(
-		@PathVariable Long id, @RequestBody FolderDto.UpdateFolderReq body) {
-		FolderDto.ReadFolderRes res = folderService.update(id, body);
-		return ResponseEntity.ok(res);
-	}
+        List<FolderDto.ReadFolderRes> folders = folderService.readAll(userId);
 
-	@ApiOperation("다음 폴더 색상 조회")
-	@GetMapping("/color")
-	public ResponseEntity<FolderDto.GetNextFolderColorRes> getNextFolderColor(
-		@LoginUserId Long userId) {
-		FolderDto.GetNextFolderColorRes res = folderService.getNextColor(userId);
-		return ResponseEntity.ok(res);
-	}
+        return ApiResult.OK(folders);
+    }
+
+    @ApiOperation("폴더 수정")
+    @PutMapping("/{id}")
+    public ApiResult<FolderDto.ReadFolderRes> update(
+            @PathVariable Long id, @RequestBody FolderDto.UpdateFolderReq body) {
+
+        FolderDto.ReadFolderRes res = folderService.update(id, body);
+
+        return ApiResult.OK(res);
+    }
+
+    @ApiOperation("다음 폴더 색상 조회")
+    @GetMapping("/color")
+    public ApiResult<FolderDto.GetNextFolderColorRes> getNextFolderColor(@LoginUserId Long userId) {
+
+        FolderDto.GetNextFolderColorRes res = folderService.getNextColor(userId);
+
+        return ApiResult.OK(res);
+    }
 }
