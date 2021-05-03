3443c7b742413bc58c84a2b9b00423ec6c3dad28
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-05-03 22:51:26 +0900

    fix: 리뷰 의견 반영

--- src/main/java/com/undertheriver/sgsg/foler/service/FolderService.java
+++ src/main/java/com/undertheriver/sgsg/foler/service/FolderService.java
@@ -1,7 +1,6 @@
 package com.undertheriver.sgsg.foler.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Sort;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.undertheriver.sgsg.common.dto.PageRequest;
+import com.undertheriver.sgsg.common.exception.ModelNotFoundException;
 import com.undertheriver.sgsg.config.PagingConfig;
 import com.undertheriver.sgsg.foler.domain.Folder;
 import com.undertheriver.sgsg.foler.domain.FolderColor;
@@ -27,15 +27,17 @@ public class FolderService {
 	public Folder save(Long userId, FolderDto.CreateFolderReq req) {
 		Integer limit = pagingConfig.getFolderConfig().get("limit");
 
-		if (isFoldersExistsMoreThan20(userId, limit)) {
+		if (foldersExistsMoreThanLimit(userId, limit)) {
 			throw new IndexOutOfBoundsException(
 				String.format("폴더는 최대 %d개까지 생성할 수 있습니다!", limit));
 		}
-		return folderRepository.save(req.toEntity());
+
+		Folder folder = req.toEntity();
+		return folderRepository.save(folder);
 	}
 
-	private boolean isFoldersExistsMoreThan20(Long userId, Integer limit) {
-		return readAll(userId).size() >= limit;
+	private boolean foldersExistsMoreThanLimit(Long userId, Integer limit) {
+		return folderRepository.countByUserIdAndDeletedFalseOrDeletedNull(userId) >= limit;
 	}
 
 	@Transactional(readOnly = true)
@@ -51,14 +53,17 @@ public class FolderService {
 
 	@Transactional(readOnly = true)
 	public Folder read(Long folderId) {
-		return folderRepository.findById(folderId).get();
+		return folderRepository.findById(folderId)
+			.orElseThrow(ModelNotFoundException::new);
 	}
 
 	@Transactional
 	public FolderDto.ReadFolderRes update(Long folderId, FolderDto.UpdateFolderReq dto) {
-		final Folder folder = folderRepository.findById(folderId).get();
+		final Folder folder = folderRepository.findById(folderId)
+			.orElseThrow(ModelNotFoundException::new);
 		folder.update(dto);
-		return FolderDto.ReadFolderRes.toDto(folder);
+		return FolderDto.ReadFolderRes
+			.toDto(folder);
 	}
 
 	@Transactional(readOnly = true)
