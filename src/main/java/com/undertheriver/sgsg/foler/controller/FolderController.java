b7ce8ba0d609ef5a737649e307915c774ac88f69
hongbin.kim <hongbin.kim@myrealtrip.com>
2021-05-09 15:59:11 +0900

    chore: 컴파일 안되는 부분 수정

--- src/main/java/com/undertheriver/sgsg/foler/controller/FolderController.java
+++ src/main/java/com/undertheriver/sgsg/foler/controller/FolderController.java
@@ -3,7 +3,6 @@ package com.undertheriver.sgsg.foler.controller;
 import java.net.URI;
 import java.util.List;
 
-import com.undertheriver.sgsg.common.ApiResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.undertheriver.sgsg.common.annotation.LoginUserId;
+import com.undertheriver.sgsg.core.ApiResult;
 import com.undertheriver.sgsg.foler.domain.dto.FolderDto;
 import com.undertheriver.sgsg.foler.service.FolderService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -25,49 +26,49 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/folders")
 @Api(value = "folder")
 public class FolderController {
-    private final FolderService folderService;
+	private final FolderService folderService;
 
-    @ApiOperation(value = "폴더 생성")
-    @PostMapping
-    public ApiResult<?> save(
-            @LoginUserId Long userId,
-            @RequestBody FolderDto.CreateFolderReq dto) {
-        try {
-            Long id = folderService.save(userId, dto);
-            URI location = new URI("/api/folders/" + id);
-            return ApiResult.OK(location);
-        } catch (IndexOutOfBoundsException e) {
-            return ApiResult.ERROR(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
-        } catch (Exception e) {
-            return ApiResult.ERROR(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
+	@ApiOperation(value = "폴더 생성")
+	@PostMapping
+	public ApiResult<?> save(
+		@LoginUserId Long userId,
+		@RequestBody FolderDto.CreateFolderReq dto) {
+		try {
+			Long id = folderService.save(userId, dto);
+			URI location = new URI("/api/folders/" + id);
+			return ApiResult.OK(location);
+		} catch (IndexOutOfBoundsException e) {
+			return ApiResult.ERROR(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+		} catch (Exception e) {
+			return ApiResult.ERROR(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
 
-    @ApiOperation("폴더 조회")
-    @GetMapping
-    public ApiResult<List<FolderDto.ReadFolderRes>> read(@LoginUserId Long userId) {
+	@ApiOperation("폴더 조회")
+	@GetMapping
+	public ApiResult<List<FolderDto.ReadFolderRes>> read(@LoginUserId Long userId) {
 
-        List<FolderDto.ReadFolderRes> folders = folderService.readAll(userId);
+		List<FolderDto.ReadFolderRes> folders = folderService.readAll(userId);
 
-        return ApiResult.OK(folders);
-    }
+		return ApiResult.OK(folders);
+	}
 
-    @ApiOperation("폴더 수정")
-    @PutMapping("/{id}")
-    public ApiResult<FolderDto.ReadFolderRes> update(
-            @PathVariable Long id, @RequestBody FolderDto.UpdateFolderReq body) {
+	@ApiOperation("폴더 수정")
+	@PutMapping("/{id}")
+	public ApiResult<FolderDto.ReadFolderRes> update(
+		@PathVariable Long id, @RequestBody FolderDto.UpdateFolderReq body) {
 
-        FolderDto.ReadFolderRes res = folderService.update(id, body);
+		FolderDto.ReadFolderRes res = folderService.update(id, body);
 
-        return ApiResult.OK(res);
-    }
+		return ApiResult.OK(res);
+	}
 
-    @ApiOperation("다음 폴더 색상 조회")
-    @GetMapping("/color")
-    public ApiResult<FolderDto.GetNextFolderColorRes> getNextFolderColor(@LoginUserId Long userId) {
+	@ApiOperation("다음 폴더 색상 조회")
+	@GetMapping("/color")
+	public ApiResult<FolderDto.GetNextFolderColorRes> getNextFolderColor(@LoginUserId Long userId) {
 
-        FolderDto.GetNextFolderColorRes res = folderService.getNextColor(userId);
+		FolderDto.GetNextFolderColorRes res = folderService.getNextColor(userId);
 
-        return ApiResult.OK(res);
-    }
+		return ApiResult.OK(res);
+	}
 }
--- src/main/java/com/undertheriver/sgsg/memo/controller/MemoController.java
+++ src/main/java/com/undertheriver/sgsg/memo/controller/MemoController.java
@@ -2,7 +2,6 @@ package com.undertheriver.sgsg.memo.controller;
 
 import java.net.URI;
 
-import com.undertheriver.sgsg.common.ApiResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.undertheriver.sgsg.common.annotation.LoginUserId;
+import com.undertheriver.sgsg.core.ApiResult;
 import com.undertheriver.sgsg.memo.domain.dto.MemoDto;
 import com.undertheriver.sgsg.memo.domain.dto.response.ReadMemoResponseDto;
 import com.undertheriver.sgsg.memo.service.MemoService;
@@ -31,66 +31,66 @@ import lombok.RequiredArgsConstructor;
 @Api(value = "memo")
 public class MemoController {
 
-    private final MemoService memoService;
+	private final MemoService memoService;
 
-    @ApiOperation(value = "메모 전체 조회")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "메모 전체 조회", response = ReadMemoResponseDto.class, responseContainer = "List")
-    })
-    @GetMapping
-    public void readAll() {
+	@ApiOperation(value = "메모 전체 조회")
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "메모 전체 조회", response = ReadMemoResponseDto.class, responseContainer = "List")
+	})
+	@GetMapping
+	public void readAll() {
 
-    }
+	}
 
-    @ApiOperation(value = "특정 폴더 메모 전체 조회")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "folderId", value = "폴더 ID", required = true, dataType = "Long", paramType = "path variable")
-    })
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "노션 서버Wiki에 메모 read 부분 참고", response = ReadMemoResponseDto.class, responseContainer = "List")
-    })
-    @GetMapping("/folders/{id}")
-    public void readAllByFolder(@PathVariable Long folderId) {
-    }
+	@ApiOperation(value = "특정 폴더 메모 전체 조회")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "folderId", value = "폴더 ID", required = true, dataType = "Long", paramType = "path variable")
+	})
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "노션 서버Wiki에 메모 read 부분 참고", response = ReadMemoResponseDto.class, responseContainer = "List")
+	})
+	@GetMapping("/folders/{id}")
+	public void readAllByFolder(@PathVariable Long folderId) {
+	}
 
-    @ApiOperation(value = "메모 단일 조회", response = ReadMemoResponseDto.class)
-    @GetMapping("/{id}")
-    public void read(@PathVariable Long id) {
+	@ApiOperation(value = "메모 단일 조회", response = ReadMemoResponseDto.class)
+	@GetMapping("/{id}")
+	public void read(@PathVariable Long id) {
 
-    }
+	}
 
-    @ApiOperation(value = "메모 저장")
-    @PostMapping
-    public ApiResult<?> save(
-            @LoginUserId Long userId,
-            @RequestBody MemoDto.CreateMemoReq body) {
-        try {
-            Long id = memoService.save(userId, body).getId();
-            URI location = new URI("/api/v1/memos/" + id);
-            return ApiResult.OK(location);
-        } catch (IndexOutOfBoundsException e) {
-            return ApiResult.ERROR(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
-        } catch (Exception e) {
-            return ApiResult.ERROR(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
+	@ApiOperation(value = "메모 저장")
+	@PostMapping
+	public ApiResult<?> save(
+		@LoginUserId Long userId,
+		@RequestBody MemoDto.CreateMemoReq body) {
+		try {
+			Long id = memoService.save(userId, body).getId();
+			URI location = new URI("/api/v1/memos/" + id);
+			return ApiResult.OK(location);
+		} catch (IndexOutOfBoundsException e) {
+			return ApiResult.ERROR(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+		} catch (Exception e) {
+			return ApiResult.ERROR(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
 
-    @ApiOperation(value = "메모 내용 수정")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "메모 ID", required = true, dataType = "Long", paramType = "path variable"),
-            @ApiImplicitParam(name = "content", value = "메모 내용", required = true, dataType = "String", paramType = "body")
-    })
-    @PutMapping("/{id}/content")
-    public void update(@PathVariable Long id, @RequestBody String content) {
-    }
+	@ApiOperation(value = "메모 내용 수정")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id", value = "메모 ID", required = true, dataType = "Long", paramType = "path variable"),
+		@ApiImplicitParam(name = "content", value = "메모 내용", required = true, dataType = "String", paramType = "body")
+	})
+	@PutMapping("/{id}/content")
+	public void update(@PathVariable Long id, @RequestBody String content) {
+	}
 
-    @ApiOperation(value = "메모 상태 수정 (토글? 명시?)")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "메모 ID", required = true, dataType = "Long", paramType = "path variable"),
-            @ApiImplicitParam(name = "status", value = "비밀 여부", required = true, dataType = "Boolean", paramType = "body")
-    })
-    @PutMapping("/{id}/status")
-    public void updateStatus(@PathVariable Long id, @RequestBody Boolean status) {
-    }
+	@ApiOperation(value = "메모 상태 수정 (토글? 명시?)")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id", value = "메모 ID", required = true, dataType = "Long", paramType = "path variable"),
+		@ApiImplicitParam(name = "status", value = "비밀 여부", required = true, dataType = "Boolean", paramType = "body")
+	})
+	@PutMapping("/{id}/status")
+	public void updateStatus(@PathVariable Long id, @RequestBody Boolean status) {
+	}
 
 }
