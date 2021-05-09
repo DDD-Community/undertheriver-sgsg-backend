16f1c334a902598e5cfd930f1ce4336193740ff3
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-05-09 23:49:26 +0900

    feat: 메모 조회 레포지토리 메서드 추가

--- src/main/java/com/undertheriver/sgsg/memo/repository/MemoRepository.java
+++ src/main/java/com/undertheriver/sgsg/memo/repository/MemoRepository.java
@@ -1,6 +1,9 @@
 package com.undertheriver.sgsg.memo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.undertheriver.sgsg.foler.domain.Folder;
@@ -8,4 +11,10 @@ import com.undertheriver.sgsg.memo.domain.Memo;
 
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    @Query(value = "SELECT m FROM Memo m "
+        + "JOIN Folder f ON f.user.id = :userId "
+        + "WHERE f.id = m.folder.id AND (m.deleted = null OR m.deleted = false) "
+        + "ORDER BY m.favorite DESC")
+    List<Memo> findAllByUserId(Long userId);
 }
