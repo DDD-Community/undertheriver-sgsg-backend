d3af7a7d7d864526635c2aed30fc6dc7b6fbe7d0
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-05-22 21:42:40 +0900

    hotfix: CORS 설정 추가

---
+++ src/main/java/com/undertheriver/sgsg/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.undertheriver.sgsg.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private static final long MAX_AGE_SECS = 3600L;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS);
+    }
+}
