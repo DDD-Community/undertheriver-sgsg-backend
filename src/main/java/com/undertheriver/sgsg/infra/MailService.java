24f946b86ecb72fd69b256dd4ca6c4de995f35a8
hongbin23 <khbasd@gmail.com>
2021-05-25 02:01:18 +0900

    style: 포매팅 수정

--- src/main/java/com/undertheriver/sgsg/infra/MailService.java
+++ src/main/java/com/undertheriver/sgsg/infra/MailService.java
@@ -15,31 +15,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MailService {
 
-	public static final String DEFAULT_PATH = ".";
-	private static final String SGSG_MAIL = "sgsg.space@gmail.com";
-	private static final String DEFAULT_ENCODING = "UTF-8";
+    public static final String DEFAULT_PATH = ".";
+    private static final String SGSG_MAIL = "sgsg.space@gmail.com";
+    private static final String DEFAULT_ENCODING = "UTF-8";
 
-	private final JavaMailSender javaMailSender;
-	private final TemplateService templateService;
+    private final JavaMailSender javaMailSender;
+    private final TemplateService templateService;
 
-	public void send(String title, String recipientEmail, Map<String, Object> models) {
-		send(DEFAULT_PATH, title, recipientEmail, models);
-	}
+    public void send(String title, String recipientEmail, Map<String, Object> models) {
+        send(DEFAULT_PATH, title, recipientEmail, models);
+    }
 
-	public void send(String templatePath, String title, String recipientEmail, Map<String, Object> models) {
-		String template = templateService.createTemplate(templatePath, models);
-		MimeMessage message = javaMailSender.createMimeMessage();
+    public void send(String templatePath, String title, String recipientEmail, Map<String, Object> models) {
+        String template = templateService.createTemplate(templatePath, models);
+        MimeMessage message = javaMailSender.createMimeMessage();
 
-		try {
-			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, DEFAULT_ENCODING);
-			mimeMessageHelper.setFrom(SGSG_MAIL);
-			mimeMessageHelper.setTo(recipientEmail);
-			mimeMessageHelper.setSubject(title);
-			mimeMessageHelper.setText(template, true);
-		} catch (MessagingException e) {
-			throw new IllegalArgumentException();
-		}
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, DEFAULT_ENCODING);
+            mimeMessageHelper.setFrom(SGSG_MAIL);
+            mimeMessageHelper.setTo(recipientEmail);
+            mimeMessageHelper.setSubject(title);
+            mimeMessageHelper.setText(template, true);
+        } catch (MessagingException e) {
+            throw new IllegalArgumentException();
+        }
 
-		javaMailSender.send(message);
-	}
+        javaMailSender.send(message);
+    }
 }
