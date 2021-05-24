e5328d7bb174deb8ea383930e3d5c1cce9ee202f
hongbin23 <khbasd@gmail.com>
2021-05-25 01:51:53 +0900

    fix: 테스트코드 삭제

--- src/test/java/com/undertheriver/sgsg/user/controller/UserControllerTest.java
+++ src/test/java/com/undertheriver/sgsg/user/controller/UserControllerTest.java
@@ -1,20 +1,15 @@
 package com.undertheriver.sgsg.user.controller;
 
-import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.undertheriver.sgsg.acceptance.AcceptanceTest;
 import com.undertheriver.sgsg.user.controller.dto.FolderPasswordRequest;
 import com.undertheriver.sgsg.user.controller.dto.UserResponseDto;
-import com.undertheriver.sgsg.user.domain.User;
 
 class UserControllerTest extends AcceptanceTest {
 
@@ -61,43 +56,4 @@ class UserControllerTest extends AcceptanceTest {
 
         assertThat(response.getHasFolderPassword()).isTrue();
     }
-
-    @DisplayName("비밀번호 초기화 요청시 비밀번호가 없으면 예외가 발생한다")
-    @Test
-    void requestInitializePasswordException() {
-        //@formatter:off
-        given()
-        .when()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .auth()
-            .oauth2(getToken())
-            .post("/v1/users/me/request-password-initialize")
-        .then()
-            .log()
-            .all()
-            .statusCode(HttpStatus.SC_BAD_REQUEST)
-            .body(containsString("이미 패스워드가 없습니다."));
-        //@formatter:on
-    }
-
-    @DisplayName("비밀번호 초기화 요청")
-    @Test
-    void requestInitializePassword() {
-        User user = getUser();
-        user.createFolderPassword("1234");
-
-        post("/v1/users/me/request-password-initialize", "");
-    }
-
-    @Test
-    void initializePassword() {
-        User user = getUser();
-        user.createFolderPassword("1234");
-
-        post("/v1/users/me/request-password-initialize", "");
-
-    }
-
-    //initializePassword
 }
