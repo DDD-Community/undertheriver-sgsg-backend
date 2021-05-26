bf7f030a429a43f21e731435348e7a0b47701528
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-05-27 00:07:50 +0900

    feat: 쿼리 수정

--- src/main/java/com/undertheriver/sgsg/memo/repository/MemoRepository.java
+++ src/main/java/com/undertheriver/sgsg/memo/repository/MemoRepository.java
@@ -12,14 +12,15 @@ import com.undertheriver.sgsg.memo.domain.Memo;
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
-    @Query(value = "SELECT m FROM Memo m "
-        + "JOIN Folder f ON f.user.id = :userId "
+    @Query(value = "SELECT m, f FROM Memo m "
+        + "JOIN Folder f ON f.user.id = :userId AND (f.deleted = null OR f.deleted = false) "
         + "WHERE f.id = m.folder.id AND (m.deleted = null OR m.deleted = false) "
         + "ORDER BY m.favorite DESC")
     List<Memo> findAllByUser(Long userId);
 
-    @Query(value = "SELECT m FROM Memo m "
-        + "WHERE m.folder.id = :folderId AND (m.deleted = null OR m.deleted = false) "
+    @Query(value = "SELECT m, f FROM Memo m "
+        + "JOIN Folder f ON m.folder.id = :folderId AND (f.deleted = null OR f.deleted = false) "
+        + "WHERE m.deleted = null OR m.deleted = false "
         + "ORDER BY m.favorite DESC")
     List<Memo> findAllByFolder(Long folderId);
 }
