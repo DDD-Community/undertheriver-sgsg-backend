c1963f5bda019b5491c0d0f935f390224ebe3498
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-06-04 21:00:23 +0900

    chore: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-REPOSITORY=/home/ubuntu/server
-cd $REPOSITORY
-
-APP_NAME=sgsg
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "> 종료할 것 없음." >>/home/ubuntu/deploy.log
-else
-  echo "> sudo kill -15 $CURRENT_PID" >>/home/ubuntu/deploy.log
-  kill -15 "$CURRENT_PID" >>/home/ubuntu/deploy.log 2>&1
-  sleep 5
-fi
-
-echo "> $JAR_PATH 배포"
-if [ "$DEPLOYMENT_GROUP_NAME" == "backend-develop" ]; then
-  nohup java -jar -Dspring.profiles.active=develop "$JAR_PATH" >>/home/ubuntu/deploy.log 2>&1 &
-elif [ "$DEPLOYMENT_GROUP_NAME" == "backend-production" ]; then
-  nohup java -jar -Dspring.profiles.active=production "$JAR_PATH" >>/home/ubuntu/deploy.log 2>&1 &
-fi
-
-echo "[$(date)] server deploy" >>/home/ubuntu/deploy.log
+LOG_PATH=/home/ubuntu/deploy.log
+echo "> 도커 컨테이너 종료" >>$LOG_PATH
+docker-compose down >>$LOG_PATH
+echo "> 도커 컨테이너 빌드/실행" >>$LOG_PATH
+docker-compose up -d --build >>$LOG_PATH
