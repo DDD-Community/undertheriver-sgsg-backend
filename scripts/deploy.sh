b5997f87b6baed82c76943d02acdf756f3402b82
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-06-07 21:52:29 +0900

    chore: 무중단 배포 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ LOG_PATH=/home/ubuntu/deploy.log
 
 if [ !$(sudo docker container ls | grep nginx) ]; then
   echo "> NGINX 컨테이너 실행" >> $LOG_PATH
+  sudo service nginx stop
   sudo docker-compose up nginx -d --build
 fi
 
-CURRENT_PORT = $(cat /etc/nginx/service_url.inc | grep -Po '[0-9]+' | gail -1)
-TARGET_PORT=0
-
+CURRENT_PORT = $(cat /etc/nginx/service_url.inc | grep -Po '[0-9]+' | tail -1)
 echo "> 현재 사용중인 포트: ${CURRENT_PORT}"
 
+TARGET_PORT=0
 if [ ${CURRENT_PORT} -eq 8080 ]; then
   TARGET_PORT = 8081
 elif [ ${CURRENT_PORT} -eq 8081 ]; then
@@ -21,13 +21,14 @@ else
 fi
 
 NEW_CONTAINER = sgsg-application-${TARGET_PORT}
-echo "> SPRINGBOOT 컨테이너 실행 | PORT: ${TARGET_PORT}" >> $LOG_PATH
+echo "> SPRINGBOOT 컨테이너 실행 | PORT: ${TARGET_PORT}"
 sudo docker-compose run -d -p ${TARGET_PORT}:8080 --name sgsg-application-${TARGET_PORT} springboot
 
 echo "> 헬스 체크"
 STARTED = 0
 for RETRY_COUNT in {1..15}; do
-  RESPONSE=$(curl -I http://localhost:8080/health)
+  echo "curl -I http://localhost:${TARGET_PORT}/health"
+  RESPONSE=$(curl -I http://localhost:${TARGET_PORT}/health)
   UP_COUNT=$(echo $RESPONSE | grep '204' | wc -l)
 
   if [ $UP_COUNT -ge 1 ]; then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
@@ -49,11 +50,10 @@ for RETRY_COUNT in {1..15}; do
 done
 
 if [ STARTED ]; then
-  echo "> 리버스 프록시 설정 변경" >> $LOG_PATH
-  echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" > /ubuntu/nginx/service_url.inc
+  echo "> 리버스 프록시 설정 변경"
+  echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | sudo tee /ubuntu/nginx/service_url.inc
   echo "> 엔진엑스 리로드"
-  sudo service nginx reload
-  echo "> 이전 버전 컨테이너 종료" >> $LOG_PATH
-  sudo docker-compose down --name sgsg-application{CURRENT_PORT}
-
-
+  sudo docker exec -it nginx service nginx reload
+  echo "> 이전 버전 컨테이너 종료"
+  sudo docker rm -f sgsg-application-${CURRENT_PORT}
+fi
