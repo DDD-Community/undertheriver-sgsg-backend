731e991ea7c46d0f3b5ab0a28f1c0eb5015aa24c
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-09 14:49:06 +0900

    feat: 코드 레벨 cors 설정 변경

--- src/main/java/com/undertheriver/sgsg/config/security/filter/CorsFilter.java
+++ src/main/java/com/undertheriver/sgsg/config/security/filter/CorsFilter.java
@@ -1,8 +1,11 @@
 package com.undertheriver.sgsg.config.security.filter;
 
+import static com.undertheriver.sgsg.common.exception.BadRequestException.NOT_ALLOWED_ORIGIN;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.FilterChain;
@@ -11,8 +14,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.undertheriver.sgsg.common.exception.BadRequestException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,17 +39,27 @@ public class CorsFilter extends OncePerRequestFilter {
         HttpServletResponse response,
         FilterChain filterChain
     ) throws ServletException, IOException {
-        String origin = request.getHeader("Origin");
 
-        if (ALLOWED_ORIGINS.contains(origin)) {
+        try {
+            String origin = request.getHeader("Origin");
+            validate(origin);
+
             response.addHeader("Access-Control-Allow-Origin", origin);
             response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
             response.addHeader("Access-Control-Allow-Headers", "Authorization");
             response.setIntHeader("Access-Control-Max-Age", 3600);
-        } else {
-            logger.info("허용되지 않은 Origin입니다");
+
+        } catch (BadRequestException e) {
+            logger.info(e.getMessage());
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void validate(String origin) {
+        if (!ALLOWED_ORIGINS.contains(origin)) {
+            String message = String.format(NOT_ALLOWED_ORIGIN, origin);
+            throw new BadRequestException(message);
+        }
     }
 }
