8824057957052abfbccc97789f30734d8f6382ec
prayme <dbfpzk142@gmail.com>
2022-01-05 21:59:27 +0900

    Preflight 처리 테스트

--- src/main/java/com/undertheriver/sgsg/config/WebMvcConfig.java
+++ src/main/java/com/undertheriver/sgsg/config/WebMvcConfig.java
@@ -10,8 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v1/**")
-            .allowedOrigins("http://localhost:3000", "https://sgsg.name","chrome-extension://mbaagcnmdjpechkaefcpcnbnmjbeddpc/popup.html", "chrome-extension://cbcfldfiodebkafgjhiokikamikajekn/popup.html")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://localhost:3000", "https://sgsg.name","chrome-extension://mbaagcnmdjpechkaefcpcnbnmjbeddpc", "chrome-extension://cbcfldfiodebkafgjhiokikamikajekn")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("Authorization", "Content-Type")
             .allowCredentials(true)
             .maxAge(3600L);
